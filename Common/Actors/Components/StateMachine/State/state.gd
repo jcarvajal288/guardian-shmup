54f91208b454eb6435207b889d6531c1461233f4
Max Carvajal <jcarvajal288@gmail.com>
2026-02-18 22:22:54 -0600

    Add StateMachine boilerplate

---
+++ Common/Actors/Components/StateMachine/State/state.gd
@@ -0,0 +1,29 @@
+class_name State extends Node
+
+@export var animation_name: String
+
+#var director: Director
+var subject: CharacterBody2D
+var signal_state_change
+
+func enter() -> void:
+	subject.animation_player.play("RESET")
+	# subject.animation_player.call_deferred("advance", 0)
+	subject.animation_player.advance(0)
+	subject.animation_player.play(animation_name)
+
+
+func exit() -> void:
+	pass
+
+
+func process_input(_event: InputEvent) -> State:
+	return null
+
+
+func process_physics(_delta: float) -> State:
+	return null
+
+
+func process_frame(_delta: float) -> State:
+	return null
