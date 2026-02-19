ce3564d932db1dd1049e0a100660872678729fac
Max Carvajal <jcarvajal288@gmail.com>
2026-02-19 16:35:11 -0600

    Snap player to pixel grid in Stand state

--- Underground/Actors/Player/States/Stand/stand.gd
+++ Underground/Actors/Player/States/Stand/stand.gd
@@ -8,6 +8,10 @@ func enter() -> void:
 
 
 func process_frame(_delta: float) -> State:
+	subject.global_position = Vector2(
+		snapped(subject.global_position.x, 1.0),
+		snapped(subject.global_position.y, 1.0)
+	)
 	if director.movement_vector.x != 0:
 		return walk_state
 	return null
