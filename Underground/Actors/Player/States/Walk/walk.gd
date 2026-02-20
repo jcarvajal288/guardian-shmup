f337390cc64f54a854ac21ea7ce3103fa6f4aaac
Max Carvajal <jcarvajal288@gmail.com>
2026-02-19 18:00:45 -0600

    Limit player movement to horizontal

--- Underground/Actors/Player/States/Walk/walk.gd
+++ Underground/Actors/Player/States/Walk/walk.gd
@@ -7,6 +7,7 @@ func process_physics(_delta: float) -> State:
 	var movement = director.movement_vector * subject.get_speed()
 	if movement == Vector2.ZERO:
 		return stand_state
+	movement.y = 0
 	subject.velocity = movement
 	subject.move_and_slide()
 	return null
