760eaea9521535a11f0bc3e2ba0a95102dc1d397
Max Carvajal <jcarvajal288@gmail.com>
2026-02-24 06:51:44 -0600

    DamageArea ticks damage continuously

--- Underground/Environment/DamageArea/damage_area.gd
+++ Underground/Environment/DamageArea/damage_area.gd
@@ -7,9 +7,23 @@ extends Area2D
 
 func _ready() -> void:
 	body_entered.connect(start_damage)
+	body_exited.connect(end_damage)
+	$Timer.timeout.connect(tick_damage)
 
 
 func start_damage(body: Node2D) -> void:
 	if body is Player:
 		is_player_in = true
-		body.take_damage(damage)
+		tick_damage()
+		$Timer.start(tick_rate)
+
+
+func tick_damage() -> void:
+	if is_player_in:
+		Global.player.take_damage(damage)
+
+
+func end_damage(body: Node2D) -> void:
+	if body is Player:
+		is_player_in = false
+		$Timer.stop()
